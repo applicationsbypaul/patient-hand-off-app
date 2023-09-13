877665fae6c327e355f8a7932b49008c282dbbda
applicationsbypaul <32409508+applicationsbypaul@users.noreply.github.com>
2023-09-12 23:19:07 -0500

    Created Navbar will work with css later

---
+++ app/components/Navbar.js
@@ -0,0 +1,35 @@
+import Link from "next/link";
+
+const Navbar = () => {
+  return (
+    <div className="navbar bg-neutral text-neutral-content">
+      <div className="flex-1">
+        <Link href="/" className="btn btn-primary normal-case text-xl">
+          HandOffApp
+        </Link>
+      </div>
+      <div className="flex-none">
+        <ul className="menu menu-horizontal px-1">
+          <li>
+            <Link href="/about">Handoffs</Link>
+          </li>
+          <li>
+            <details>
+              <summary>Admin</summary>
+              <ul className="p-2 bg-base-100">
+                <li>
+                  <a>Employees</a>
+                </li>
+                <li>
+                  <a>Patients</a>
+                </li>
+              </ul>
+            </details>
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
